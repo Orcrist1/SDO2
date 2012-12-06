0307e90dc9040a98be2ccaf2accd611fd47fde12
swallx <echelon42@hotmail.fr>
2012-12-06 14:00:40 +0100

    suppression des commentaires !

--- myRandom.h
+++ myRandom.h
@@ -1,8 +1,7 @@
 #ifndef MYRANDOM_H_INCLUDED
 #define MYRANDOM_H_INCLUDED
 #include <stdbool.h>
-/* ce fichier contient des fonctions qui permets l'exclusion automatique des nombres qui ont déjà
-été ressortis par une de ses fonctions*/
+
 typedef struct randomNumber_t
 {
     unsigned int* pNumber;
